cbb090892d5746dee3fb09900adc77f5ed514f56
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-10-08 18:58:29 -0500

    Update - Controller Part I

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
